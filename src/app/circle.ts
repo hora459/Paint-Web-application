7e647445204acdc8ff1d1a01ba039ccd94bf174a
Ahmed Samir <modysamir459@gmail.com>
2022-12-13 00:45:42 +0200

    front-end bfore undo and redo

---
+++ src/app/circle.ts
@@ -0,0 +1,12 @@
+export class Circle {
+    x=0;
+    y=0;
+    radius=30;
+    stroke='black';
+    strokeWidth=2;
+    scaleX=1;
+    scaleY=1;
+    type='circle';
+    id=0;
+    fill='';
+}
---
+++ src/app/triangle.ts
@@ -0,0 +1,13 @@
+export class Triangle {
+    x=0;
+    y=0;
+    sides=3;
+    radius=20;
+    stroke='black';
+    scaleX=1;
+    scaleY=1;
+    strokeWidth=2;
+    rotation_angle=0;
+    type='triangle';
+    id=0;
+}
